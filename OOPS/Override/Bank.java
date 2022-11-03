28814e7886dbac0300e9a72f12b3856df49da631
HAREESH BHITTAM <72018706+Hareesh108@users.noreply.github.com>
2022-11-03 21:51:52 +0530

    Add files via upload

---
+++ OOPS/Override/Bank.java
@@ -0,0 +1,38 @@
+package Override;
+
+//Java Program to demonstrate the real scenario of Java Method Overriding
+//where three classes are overriding the method of a parent class.
+
+//Creating a parent class.
+class Bank{
+    int getRateOfInterest(){return 0;}
+}
+
+//Creating child classes.
+class SBI extends Bank{
+    int getRateOfInterest(){return 8;}
+}
+
+class ICICI extends Bank{
+    int getRateOfInterest(){return 7;}
+}
+class AXIS extends Bank{
+    int getRateOfInterest(){return 9;}
+}
+
+//Test class to create objects and call the methods
+class Test2{
+    public static void main(String args[]){
+        SBI s=new SBI();
+        ICICI i=new ICICI();
+        AXIS a=new AXIS();
+        System.out.println("SBI Rate of Interest: "+s.getRateOfInterest());
+        System.out.println("ICICI Rate of Interest: "+i.getRateOfInterest());
+        System.out.println("AXIS Rate of Interest: "+a.getRateOfInterest());
+    }
+}
+
+
+//Why can we not override static method?
+//It is because the static method is bound with class whereas instance method is bound with an object.
+// Static belongs to the class area, and an instance belongs to the heap area.
